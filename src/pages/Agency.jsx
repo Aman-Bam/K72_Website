1c5aea91f546c3c8204ae40743634a60a3d345c8
Aman-Bam <mhub7058@gmail.com>
2025-09-16 23:53:01 +0530

    scrolling done

--- src/pages/Agency.jsx
+++ src/pages/Agency.jsx
@@ -1,9 +1,102 @@
-import React from 'react'
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+import React, { useRef } from "react";
 
 const Agency = () => {
-  return (
-    <div>Agency</div>
-  )
-}
+  gsap.registerPlugin(ScrollTrigger);
+  const imageDivRef = useRef(null);
+  const imageRef = useRef(null);
 
-export default Agency
+  const imageArray = [
+    "https://k72.ca/uploads/teamMembers/CAMILLE_480X640_2-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/Carl_480x640-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/ChantalG_480x640-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/Claire_480x640-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/HugoJoseph_480x640-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/Claire_640X960-640x960.jpg",
+    "https://k72.ca/uploads/teamMembers/Lawrence_480x640-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/MAXIME_480X640_2-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/Michele_480X640-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/MyleneS_480x640-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/Olivier_480x640-480x640.jpg",
+    "https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290-640x290.jpg",
+
+    "https://k72.ca/uploads/teamMembers/CAMILLE_480X640_2-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/Carl_480x640-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/ChantalG_480x640-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/Claire_480x640-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/HugoJoseph_480x640-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/Claire_640X960-640x960.jpg",
+    "https://k72.ca/uploads/teamMembers/Lawrence_480x640-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/MAXIME_480X640_2-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/Michele_480X640-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/MyleneS_480x640-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/Olivier_480x640-480x640.jpg",
+    "https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290-640x290.jpg",
+  ];
+
+  useGSAP(function () {
+    gsap.to(imageDivRef.current, {
+      scrollTrigger: {
+        trigger: imageDivRef.current,
+        markers: true,
+        start: "top 28.3%",
+        end: "top -142%",
+        scrub: true,
+        pin: true,
+        onUpdate: (elem) => {
+          let imageIndex;
+          if (elem.progress < 1) {
+            imageIndex = Math.floor(elem.progress * imageArray.length);
+          } else {
+            imageIndex = imageArray.length - 1;
+          }
+
+          imageRef.current.src = imageArray[imageIndex];
+        },
+      },
+    });
+  });
+
+  return (
+    <div>
+      <div className="section1">
+        <div
+          ref={imageDivRef}
+          className="absolute overflow-hidden h-[20vw] rounded-4xl w-[15vw] top-50 left-[31vw]"
+        >
+          <img
+            ref={imageRef}
+            className="h-full object-cover w-full "
+            src="https://k72.ca/uploads/teamMembers/Carl_480x640-480x640.jpg"
+            alt=""
+          />
+        </div>
+        <div className="relative font-[font2]">
+          <div className="mt-[55vh]">
+            <h1 className="text-[20vw] text-center uppercase leading-[18vw]">
+              Soixan7t
+              <br />
+              Twelve
+            </h1>
+            <div className="pl-[40%] mt-20 ">
+              <p className="text-6xl">
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Our
+                curiosity fuels our creativity. We remain humble and say no to
+                big egos, even yours. A brand is alive. She has values, a
+                personality, a history. If we forget that, we can make good
+                numbers in the short term, but we kill it in the long term. This
+                is why we are committed to giving perspective, to building
+                influential brands.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="section2 h-screen"></div>
+    </div>
+  );
+};
+
+export default Agency;
