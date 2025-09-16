9edd9cfd378b22669531c3790a9ae6d1d64c9a94
Aman-Bam <mhub7058@gmail.com>
2025-09-16 20:04:23 +0530

    homebottomtext done

--- src/Components/home/Homebottomtext.jsx
+++ src/Components/home/Homebottomtext.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Homebottomtext = () => {
   return (
-    <div>Homebottomtext</div>
+  <div className='font-[font2] flex items-center justify-center gap-3'>
+   <div className='border-3 hover:border-[#D3FD50] hover:text-[#D3FD50] h-23 flex items-center px-14 border-white rounded-full uppercase'>
+     <Link className='text-[6vw] mt-6' to='/project'>Projects</Link>
+     </div>
+
+     <div className='border-3 hover:border-[#D3FD50] hover:text-[#D3FD50] h-23 flex items-center px-14 border-white rounded-full uppercase'>
+    <Link className='text-[6vw] mt-6'to='/agency'> Agence</Link>
+    </div>
+  </div>
   )
 }
 
